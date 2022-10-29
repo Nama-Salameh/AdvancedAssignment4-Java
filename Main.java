19147e6ab6f4985825f2a0b49392f55cb826936c
“Nama-Salamehgit config --global user.email “nama.salameh@gmail.com <92352860+Nama-Salameh@users.noreply.github.com>
2022-10-29 17:29:31 +0300

    Solution

--- Main.java
+++ Main.java
@@ -6,19 +6,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi");
         //Deep copy
-        Color deeplCopyRed = ColorStore.getCopy("255-0-0");
+        /**Color deeplCopyRed = ColorStore.getCopy("255-0-0");
         System.out.println("red " + ColorStore.colors.get("255-0-0"));//255,0,0
         System.out.println("copy " + deeplCopyRed);//255,0,0
 
         System.out.println("red " + ColorStore.colors.get("255-0-0"));//255,0,0 ?
         System.out.println("copy " + deeplCopyRed);//255,1,1 ok
 
-        //ColorStore.colors.get("255-0-0").setRed(240);
-
         System.out.println("red " + ColorStore.colors.get("255-0-0"));//240,1,1 ?
         System.out.println("copy " + deeplCopyRed);//240,1,1 ok
-
-        System.out.println("hhh " + ColorStore.colors.get("240-5-5"));
+**/
+        System.out.println(Color.getColor(255,255,0));
+        System.out.println(Color.getColor(255,255,255));
+        System.out.println(Color.getColor(240,255,1));
         System.out.println(ColorStore.colors);
     }
 }
--- intf/Color.java
+++ intf/Color.java
@@ -1,6 +1,6 @@
 package intf;
 
-public class Color implements Cloneable{
+public class Color implements Cloneable {
     private final int red;
     private final int green;
     private final int blue;
@@ -8,59 +8,51 @@ public class Color implements Cloneable{
     static {
         System.out.println("I am scaning Color class");
     }
+
     public Color(int red, int green, int blue) {
         this.red = red;
         this.green = green;
         this.blue = blue;
     }
 
-    public Color(Color color){
+    public Color(Color color) {
         this.red = color.red;
         this.green = color.green;
         this.blue = color.blue;
     }
 
-    public int getRed() {
-        return red;
-    }
-
-    public int getGreen() {
-        return green;
-    }
-
-    public int getBlue() {
-        return blue;
+    @Override
+    public String toString() {
+        return " " + this.red + "-" + this.green + "-" + this.blue + " ";
     }
 
     @Override
-    public String toString(){
-        return " " + this.red + "," + this.green + "," + this.blue;
-    }
-
-    @Override
-    public Object clone(){
+    public Object clone() {
         Object object = null;
         try {
             object = super.clone();
-        } catch (CloneNotSupportedException e){
+        } catch (CloneNotSupportedException e) {
             System.err.println(e.getMessage());
         }
         return object;
 
     }
 
-    public static String convert(int red, int green, int blue){
+    public static String convert(int red, int green, int blue) {
         return String.valueOf(red) + "-" + String.valueOf(green) + "-" + String.valueOf(blue);
     }
 
     //2 & 3
-    public static Color getColor(int red, int green, int blue){
-        String colorstr = convert(red, green, blue);
-        if(!ColorStore.colors.containsKey(colorstr)){
-            Color color = new Color(red,green,blue);
-            ColorStore.colors.put(colorstr,color);
+    public static Color getColor(int red, int green, int blue) {
+        String colorstr = Color.convert(red, green, blue);
+        if (!ColorStore.colors.containsKey(colorstr)) {
+            Color color = new Color(red, green, blue);
+            ColorStore.colors.put(colorstr, color);
+            System.out.print("This color is defined now : ");
             return color;
-        }else
+        } else {
+            System.out.print("This color already exists : ");
             return ColorStore.getCopy(colorstr);
+        }
     }
 }
